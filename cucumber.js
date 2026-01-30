e45b897de4d1bfe60f0c9299747ee60cf1d04732
vadsandy <sandeep.dct@gmail.com>
2026-01-30 17:16:29 +0530

    new2

--- cucumber.js
+++ cucumber.js
@@ -7,11 +7,13 @@ module.exports = {
     paths: [
       'src/features/**/*.feature'
     ],
+    // Simplified format array to avoid schema validation errors
     format: [
       'progress-bar',
-      ['allure-cucumberjs/reporter', { resultsDir: 'allure-results' }]
+      'allure-cucumberjs/reporter'
     ],
     formatOptions: {
+      resultsDir: 'allure-results',
       snippetInterface: 'async-await'
     },
     publishQuiet: true
